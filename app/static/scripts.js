129b35e2e02aadad16440145029f3566e6321558
fawnyler <125075848+fawnyler@users.noreply.github.com>
2024-06-17 18:21:53 +0300

    Delete app directory

--- app/static/scripts.js
+++
@@ -1,5 +0,0 @@
-var easyMDE = new easyMDE({
-  element: document.getElementById('descr'),
-  spellChecker: false, // Optional: disable spell checker
-  forceSync: true // Optional: force text to be updated immediately in the textarea
-});
